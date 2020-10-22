14f25ec3e4b54e955ae97452138aa474ad6fe00b
TZeuch <zeuchtony@gmail.com>
2020-10-22 17:59:04 -0500

    added login process, as well as security measure for passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -60,11 +60,43 @@ router.post('/', (req, res) =>
     });
 });
 
+// POST /api/users/login
+router.post('/login', (req, res) =>
+{
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne(
+    {
+        where: 
+        {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => 
+    {
+        if (!dbUserData) 
+        {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if(!validPassword)
+        {
+            res.status(400).json({ message: 'Incorrect Password' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });  
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) =>
 {
     User.update(req.body, 
     {
+        individualHooks: true,
         where:
         {
             id: req.params.id
