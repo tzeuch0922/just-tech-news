58a3fef31f9e6da726254dbbf489c68a32beab07
TZeuch <zeuchtony@gmail.com>
2020-10-23 19:58:29 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,20 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our database
-const sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PW,
+let sequelize;
+
+if(process.env.JAWSDB_URL)
 {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else
+{
+    sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PW,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
