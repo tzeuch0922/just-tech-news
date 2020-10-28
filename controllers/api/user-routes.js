703b822ef7c8e7e61ac94f76ca1e408816025c51
TZeuch <zeuchtony@gmail.com>
2020-10-28 15:42:18 -0500

    add login and logout procedures

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -74,7 +74,17 @@ router.post('/', (req, res) =>
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => 
+    {
+        req.session.save(() =>
+        {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err =>
     {
         console.log(err);
@@ -109,8 +119,31 @@ router.post('/login', (req, res) =>
             res.status(400).json({ message: 'Incorrect Password' });
             return;
         }
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+        req.session.save(() =>
+        {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });  
+});
+
+router.post('/logout', (req, res) =>
+{
+    if(req.session.loggedIn)
+    {
+        req.session.destroy(() =>
+        {
+            res.status(204).end();
+        });
+    }
+    else
+    {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
