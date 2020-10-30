da0c0e75e60013db90945f25ae1e75d9d6358f09
TZeuch <zeuchtony@gmail.com>
2020-10-30 16:52:16 -0500

    added helper functions and implemented them in partials

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers.js');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const session = require('express-session');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
